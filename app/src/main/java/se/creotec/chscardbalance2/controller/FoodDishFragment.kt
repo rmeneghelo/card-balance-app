c2ffbef34a74f84b6741d90beea649158459590b
Rafael Zambelli Meneghelo <zaamba@gmail.com>
2017-10-18 22:40:26 -0200

    Replace findViewById in FoodDishFragment

--- app/src/main/java/se/creotec/chscardbalance2/controller/FoodDishFragment.kt
+++ app/src/main/java/se/creotec/chscardbalance2/controller/FoodDishFragment.kt
@@ -9,11 +9,11 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_dish_list.*
 import se.creotec.chscardbalance2.R
 import se.creotec.chscardbalance2.model.Dish
 import se.creotec.chscardbalance2.model.Restaurant
@@ -33,18 +33,16 @@ class FoodDishFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_dish_list, container, false)
-        val recyclerView = view.findViewById(R.id.dish_list) as RecyclerView
 
         // Set the adapter
         val layoutManager = LinearLayoutManager(context)
-        recyclerView.layoutManager = layoutManager
-        val dividerLine = DividerItemDecoration(recyclerView.context, layoutManager.orientation)
-        recyclerView.addItemDecoration(dividerLine)
-        recyclerView.adapter = FoodDishRecyclerViewAdapter(restaurant.dishes, dishListener)
+        dish_list.layoutManager = layoutManager
+        val dividerLine = DividerItemDecoration(dish_list.context, layoutManager.orientation)
+        dish_list.addItemDecoration(dividerLine)
+        dish_list.adapter = FoodDishRecyclerViewAdapter(restaurant.dishes, dishListener)
 
         if (restaurant.isClosed()) {
-            val emptyView = view.findViewById(R.id.restaurant_closed_view)
-            emptyView.visibility = View.VISIBLE
+            restaurant_closed_view.visibility = View.VISIBLE
         }
         return view
     }
